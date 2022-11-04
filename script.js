dba90a0c86212e4d7837696796a1829faa0957f2
Dmitrii <D1Fol@yandex.ru>
2022-11-05 00:38:15 +0500

    swiper 1.5
    swiper 5.11.22

---
+++ script.js
@@ -0,0 +1,35 @@
+const swiper = new Swiper('.swiper', 
+{
+    direction: 'horizontal',
+    loop: true,
+    pagination: {el: '.swiper-pagination'},
+    slidesPerView: 1.25
+}
+);
+
+let cardContentWrapper = document.querySelector(".brands-menu-wrap");
+let showDown = document.querySelector('.show-down-btn');
+let showDownImg = document.querySelector('.show-down-img');
+let showContainers = document.querySelector(".show-container")
+
+// showDown.addEventListener('click', func1, showDowner);
+
+function showDowner() {
+    if(showDown.textContent === "Показать всё") {
+        showDown.textContent = "Скрыть";
+        showDownImg.classList.add('reverse');
+    } else {
+        showDown.textContent = "Показать всё";
+        showDownImg.classList.remove('reverse');
+    }
+}
+
+function func1() {
+  if (showContainers.style.display !== 'none') {
+    showContainers.style.display = "none";
+  }else {
+    showContainers.style.display = "block";
+  }
+};
+
+console.log(showContainers);
