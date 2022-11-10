ba661549bbf6d8aa9f7ae43668f2f3b7283b5989
Dmitrii <D1Fol@yandex.ru>
2022-11-11 01:38:42 +0500

    commit 11.11

--- script.js
+++ script.js
@@ -1,36 +1,32 @@
-const swiper = new Swiper('.swiper', 
-{
+new Swiper ('.swiper', {
     direction: 'horizontal',
     loop: true,
     pagination: {el: '.swiper-pagination'},
-    slidesPerView: 1.25
+    slidesPerView: 'auto',
+
 }
 );
 
 let showDown = document.querySelector('.show-down-btn');
 let showDownImg = document.querySelector('.show-down-img');
-let showContainers = document.querySelector(".show-container")
+let showContainers = document.querySelectorAll('.show-container')
 
-// showDown.addEventListener('click', func1, showDowner);
+showDown.addEventListener('click', showDowner)
+
 
 function showDowner() {
     if(showDown.textContent === "Показать всё") {
+	  for( let i = 0; i < showContainers.length; i++) { 
+	    showContainers[i].style.display = "block";
+    }
         showDown.textContent = "Скрыть";
         showDownImg.classList.add('reverse');
     } else {
+      for( let i = 0; i < showContainers.length; i++) { 
+        showContainers[i].style.display = "none";
+      }
         showDown.textContent = "Показать всё";
         showDownImg.classList.remove('reverse');
     }
 }
 
-function func1() {
-  if (showContainers.style.display !== 'none') {
-    showContainers.style.display = "none";
-  }else {
-    showContainers.style.display = "block";
-  }
-};
-
-console.log(showContainers);
-
-// Не получается реализовать показ скрытых элементов. Показывается только один и то коряво, не пойму в чем причина. Направь меня, пожалуйста
